8021518f0cd4139e283596f1dcd6597aab7ae43d
Moises Trigueros <moisest@nicasource.com>
2022-03-04 22:52:40 -0600

    add validation pipe and config service

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+async function bootstrap (): Promise<void> {
+  const app = await NestFactory.create(AppModule)
+  app.enableShutdownHooks()
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true
+      }
+    })
+  )
+
+
+  const configService = app.get(ConfigService)
+  const port = configService.get('PORT')
+  await app.listen(port)
 }
-bootstrap();
+
+bootstrap().catch((e) => console.error(e))
